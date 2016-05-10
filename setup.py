ac08822636d33e22293d6ed21885fc800a361c89
Nicolas Sandller <nicosandller@gmail.com>
2016-05-10 11:48:32 -0300

    Setup file created PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'python-ethereumrpc',
+  packages = ['python-ethereumrpc'],
+  version = '0.1',
+  description = 'A python interface for ethereum JSON-RPC service.',
+  author = 'Nicolas Sandller',
+  author_email = 'nicosandller@gmail.com',
+  url = 'https://github.com/nicosandller/python-ethereumrpc',
+  download_url = 'https://github.com/nicosandller/python-ethereumrpc/tarball/0.1',
+  keywords = ['ethereum', 'rpc', 'api', 'JSON', 'JSON-RPC'],
+  classifiers = [],
+)
